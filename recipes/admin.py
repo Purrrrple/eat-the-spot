7b037c56641dc8defc94cb2e67f0d37cc9fdff1f
Purple <cristina.delgaudio@gmail.com>
2016-04-17 15:39:22 +0200

    Add ingredient amounts inline in RecipeAdmin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, Amount, Ingredient
+
 
 # Register your models here.
-admin.site.register(Recipe)
+
+admin.site.register(Ingredient)
+
+class AmountInline(admin.TabularInline):
+    model = Amount
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [
+        AmountInline,
+    ]
+
+admin.site.register(Recipe, RecipeAdmin)
